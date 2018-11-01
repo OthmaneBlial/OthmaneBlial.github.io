42d2606e7b7d05225ec70a9c4b3e81363ec3c1cd
BLIAL OTHMANE <blial.othmane@gmail.com>
2018-11-02 00:36:28 +0100

    Add files via upload

---
+++ triangleapp/precache-manifest.ee8ea53a85e2e6879330b5a449331865.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ea22071b5849676f7566",
+    "url": "/static/js/main.ea22071b.chunk.js"
+  },
+  {
+    "revision": "e3a98e8ca5ecf102f1b6",
+    "url": "/static/js/1.e3a98e8c.chunk.js"
+  },
+  {
+    "revision": "ea22071b5849676f7566",
+    "url": "/static/css/main.2b56cab3.chunk.css"
+  },
+  {
+    "revision": "6007a9a0c13126d7a66cd7603044bb13",
+    "url": "/index.html"
+  }
+];
